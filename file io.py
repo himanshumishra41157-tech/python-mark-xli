2f52a0bc3a0979ad37daab6bc365a45e7f5b2bb5
himanshumishra41157-tech <himanshumishra41157@gmail.com>
2026-09-24 23:42:00 +0530

    Update Python practice

--- file io.py
+++ file io.py
@@ -1,27 +1,27 @@
-# agar hame kisi file ko open krke usko read krna hoto ham phle ik file 
-# banayege jisme data text ke taur par hoga and then usko f ya kisi bhi variable se assign krege after this isme ham f = open('my file.txt , 'r') f.read()
-# f = open('my file.txt' , 'r')
-# # f = open('my file.txt' , 'r')   #agar ham yaha bina r mode ke kare tb bhi chlega  q ki r mod fefault hota hai
-# text = f.read()
+# # agar hame kisi file ko open krke usko read krna hoto ham phle ik file 
+# # banayege jisme data text ke taur par hoga and then usko f ya kisi bhi variable se assign krege after this isme ham f = open('my file.txt , 'r') f.read()
+# # f = open('my file.txt' , 'r')
+# # # f = open('my file.txt' , 'r')   #agar ham yaha bina r mode ke kare tb bhi chlega  q ki r mod fefault hota hai
+# # text = f.read()
 
-# print(text)
+# # print(text)
+# # f.close()
+
+# #for write inside the file
+# f = open('my file2.txt' , 'w')
+# text = f.write('Helllo ! himanshu')
+
 # f.close()
 
-#for write inside the file
-f = open('my file2.txt' , 'w')
-text = f.write('Helllo ! himanshu')
-
-f.close()
 
 
 
 
 
 
-
-#agar hame without  close statement ka use kre file likhni hai  to ham with statement ka use krege
-with open('my file.txt' ,  'a'):
-    f.write("hey im inside with") 
+# #agar hame without  close statement ka use kre file likhni hai  to ham with statement ka use krege
+# with open('my file.txt' ,  'a'):
+#     f.write("hey im inside with") 
 
 
 
@@ -31,6 +31,12 @@ with open('my file.txt' ,  'a'):
 #agar hamne binary file read krni ho to ham rb use kreege comma ke baad
 
 
+
+# agar hame kisi file me naye text rewrite krne ho to ham use krege
+
+f = open("my file2.txt" , 'w')
+text = f.write("mera name haidon or mai hu nayab rakhwala madhyapradesh ka")
+f.close()
 
 
 
