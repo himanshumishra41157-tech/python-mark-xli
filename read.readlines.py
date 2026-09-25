379bc98608abcbbc962e79a46620206988a235d5
himanshumishra41157-tech <himanshumishra41157@gmail.com>
2026-09-25 23:26:45 +0530

    Update Python practice

--- read.readlines.py
+++ read.readlines.py
@@ -1,1 +1,8 @@
 #so today  we are discussing about the read and readlines.py
+
+f = open('intro.txt' ,  'r')
+while True:
+    lines = f.readlines()
+    if not lines:
+        break
+    print(lines)
